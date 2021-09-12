cefeca434b72a36906a1a439bb9afbf9e52a05a5
Ty <justfacey@gmail.com>
2021-09-11 21:10:43 -0400

    Added soundfont API dependency.

---
+++ src/domain/audio.ts
@@ -0,0 +1,7 @@
+import { Optional } from "./types"
+
+// Returns window audio based on user's browser.
+export function accessContext(): Optional<AudioContextType> {
+    return window.AudioContext || window.webkitAudioContext || null
+
+}
