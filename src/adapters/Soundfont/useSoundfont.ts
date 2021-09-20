25118e4f0dc864d0cfcec8573351af555f43329a
Ty <justfacey@gmail.com>
2021-09-20 08:04:08 -0400

    Created adapter to use Soundfont and window context

---
+++ src/adapters/Soundfont/useSoundfont.ts
@@ -0,0 +1,30 @@
+import { useState, useRef } from "react";
+import Soundfont, { InstrumentName, Player } from "soundfont-player";
+import { MidiValue } from "../../domain/notes";
+import { Optional } from  "../../domain/types"
+import {
+  AudioNodesRegistry,
+  DEFAULT_INSTRUMENT
+} from "../../domain/sound"
+
+type Settings = {
+    AudioContext: AudioContextType
+}
+
+interface Adapted {
+    loading: boolean,
+    current: Optional<InstrumentName>
+
+    load(instrument?: InstrumentName): Promise<void>
+    play(note: MidiValue): Promise<void>
+    stop(note: MidiValue): Promise<void>
+}
+
+export function useSoundfont({ AudioContext }: Settings): Adapted {
+    let activeNodes: AudioNodesRegistry = {}
+
+    const [current, setCurrent] = useState<Optional<InstrumentName>>()
+    const [loading, setLoading] = useState<boolean>(false)
+    const [player, setPlayer] = useState<Optional<Player>>()
+    const audio = useRef(new AudioContext())
+}
