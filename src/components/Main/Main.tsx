da79a210444a09dd298b880342e7b35c2b86da22
Ty <justfacey@gmail.com>
2021-09-23 10:33:50 -0400

    Created component for key and sound functionality.

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,8 @@
+import { KeyboardWithInstrument } from "../Keyboard/WithInstrument";
+import { NoAudioMessage } from "../NoAudioMessage/NoAudioMessage";
+import { useAudioContext } from "../AudioContextProvider/useAudioContext";
+
+export const Main = () => {
+    const AudioContext = useAudioContext()
+    return !!AudioContext ? ( <KeyboardWithInstrument /> ) : ( <NoAudioMessage />)
+}
